0ed91fb6639ccc140476429fd99d37a387253373
Fred Wu <ifredwu@gmail.com>
2012-07-02 10:07:31 +1000

    Reload routes on every action

--- app/controllers/api_taster/application_controller.rb
+++ app/controllers/api_taster/application_controller.rb
@@ -1,5 +1,13 @@
 module ApiTaster
   class ApplicationController < ActionController::Base
     layout proc { |controller| controller.request.xhr? ? nil : 'api_taster/application' }
+
+    before_filter :reload_routes
+
+    private
+
+    def reload_routes
+      load Rails.application.root.join('config/routes.rb')
+    end
   end
 end
