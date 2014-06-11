01ff73af7c6741fdaef06cfb72bc3741215efeea
Mario A Chavez <mario.chavez@gmail.com>
2014-06-11 20:19:04 +0000

    Fix failing tests Set explicit sass-rails version to help travis resolve dependencies

--- spec/route_spec.rb
+++ spec/route_spec.rb
@@ -15,9 +15,10 @@ module ApiTaster
         :name => 'home',
         :verb => 'GET',
         :path => '/home',
+        :full_path => '/home',
         :reqs => {
-          :controller => 'application',
-          :action => 'home'
+          :action => 'home',
+          :controller => 'application'
         }
       }
     end
